f17601f7044c8c9d3b4212670e7c05e47ea49d7e
Syedailsa <159128442+Syedailsa@users.noreply.github.com>
2025-01-22 06:04:17 +0500

    security-credentials

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,5 +6,6 @@ import { defineCliConfig } from 'sanity/cli'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const token = process.env.NEXT_PUBLIC_SANITY_TOKEN
 
 export default defineCliConfig({ api: { projectId, dataset } })
--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -2,17 +2,17 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-18'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  process.env.NEXT_PUBLIC_SANITY_DATASET ,
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'xscs6w0z',
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
 export const token = assertValue(
-  process.env.SANITY_AUTH_TOKEN || 'skPt8jAdYHmyYZ2qxwSyW1nlD6FZ4rtSEKT9UWFfLo0am534oa454pzX1tCmv6CQdhN9e6yMMSyJ0yOOXDfuRCAzAtwtfWx5gIs9arC0KcBUEAUtJcd4azuALzQDAVXEeB6EsOhwq5XTXoUyrgZH8esy1Gp2V8i8secKP7jTgQ5rVPlngS1w',
+  process.env.NEXT_PUBLIC_SANITY_TOKEN,
   'Missing environment variable: NEXT_PUBLIC_SANITY_TOKEN'
 )
 
